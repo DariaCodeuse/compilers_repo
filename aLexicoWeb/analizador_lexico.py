281f141dfe9ea9465b7d3a481f8e752cd56fac65
Karla Díaz <diazaguilarkarla8@gmail.com>
2024-08-21 11:17:41 -0600

    fix: rename files and variables

--- aLexicoWeb/analizador_lexico.py
+++ aLexicoWeb/analizador_lexico.py
@@ -1,7 +1,11 @@
+# \aLexicoWeb\.venv\Scripts\
+#.\activate
+
 import ply.lex as lex
 from flask import Flask, render_template, request
 
-app = Flask(_name_)
+# Instancia de App en Flask
+app = Flask(__name__)
 
 reserved = {
     'for': 'FOR',
@@ -23,20 +27,20 @@ def t_error(t):
 
 lexer = lex.lex()
 
-@app.router('/', metodos=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         code = request.form.get('code', '')
-        # lexer.input(content)
+        lexer.input(code)  # Asegúrate de usar 'code' y no 'content'
 
         result_lexema = [
-            (f"Reservada {token.type.capitalize()}" if token.type in reserved.values(
-            ) else "Parentesis de apertura" if token.type == "ABIERTO" else "Parentesis de cierre", token.value)
+            (f"Reservada {token.type.capitalize()}" if token.type in reserved.values()
+             else "Parentesis de apertura" if token.type == "PABIERTO" 
+             else "Parentesis de cierre", token.value)
             for token in lexer
         ]
         return render_template('Home.html', tokens=result_lexema)
-    return render_template('Home.html', tokens=None)
+    return render_template('index.html', tokens=None)
 
-
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(debug=True)
