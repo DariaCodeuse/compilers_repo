5c9a4f8531d63e21dbfd3549d6c075c33df5fa0c
Karla Díaz <diazaguilarkarla8@gmail.com>
2024-08-28 00:09:13 -0600

    fix: result_lexema updated for comparing values in dict

--- aLexicoWeb/analizador_lexico.py
+++ aLexicoWeb/analizador_lexico.py
@@ -1,5 +1,3 @@
-# .\.venv\Scripts\activate
-
 import ply.lex as lex
 import json
 from flask import Flask, render_template, request
@@ -11,12 +9,15 @@ app = Flask(__name__)
 with open('tokens.json') as f:
     data = json.load(f)
 
-reserved = data['reserved']
-print(reserved)
-
-tokens = list(data.keys())
+# Cargar lista de tokens
+tokens = [token for category in data.values() for token in category.values()]
+tokens.append("NUMERO")
 print(tokens)
 
+# Cargar solo NOMBRES DE TOKENS (Claves)
+typeTokens = list(data.keys())
+typeTokens.append("number")
+print(typeTokens)
 
 # Reglas para los tokens
 t_FOR = r"for"
@@ -24,34 +25,70 @@ t_DO = r'do'
 t_WHILE = r'while'
 t_IF = r'if'
 t_ELSE = r'else'
+t_PUBLIC = r'public'
 t_STATIC = r'static'
-t_VOID = r'void"'
-t_PUNTO = r'd+.d+\;?'
+t_INT = r'int'
+t_VOID = r'void'
 
-t_ignore = ' \t\n\r'
-t_PABIERTO = r'\('
-t_PCERRADO = r'\)'
+t_P_ABIERTO = r'\('
+t_P_CERRADO = r'\)'
+t_LLAVE_ABIERTA = r'\{'
+t_LLAVE_CERRADA = r'\}'  # Este nombre tenía un error, lo corregí
+t_CORCHETE_I = r'\['
+t_CORCHETE_D = r'\]'
+
+t_VAR = r'var'
+t_LET = r'let'
+t_CONST = r'const'
+t_FUNCTION = r'function'
+t_MAIN = r'main'
+
+t_IGUAL = r'='
+t_SUMA = r'\+'
+t_RESTA = r'-'
+t_MULTIPLICACION = r'\*'
+t_DIVISION_S = r'/'
+t_DIVISION_E = r'//'
+
+t_PUNTO = r'\.'  # Es necesario escapar el punto
+t_PUNTO_Y_COMA = r';'
+t_COMA = r','
+
+t_NUMERO = r'\d+'
+
+t_ignore = ' \t'
+
+# Regla para contar los números de línea
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
 def t_error(t):
-    print('Caracter no valido', t.value[0])
+    print('Caracter no válido', t.value[0])
     t.lexer.skip(1)
-
-lexer = lex.lex()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         code = request.form.get('code', '')
-        lexer.input(code)  # Asegúrate de usar 'code' y no 'content'
-
-        result_lexema = [
-            (f"Reservada {token.type.capitalize()}" if token.type in reserved.values()
-            else "Parentesis de apertura" if token.type == "PABIERTO"
-            else "Parentesis de cierre" if token.type == "PCERRADO"
-            else f"Token desconocido ({token.type})", token.value)
-            for token in lexer
-        ]
-
+        lexer = lex.lex()
+        lexer.input(code)
+        
+        result_lexema = []
+        for token in lexer:
+            descripcion = "Desconocido"
+            
+            # Convertir el valor del token a minúsculas para buscar en el JSON
+            token_value = token.value.lower()
+            
+            # Buscar la descripción del token
+            for category, tokens_dict in data.items():
+                if token_value in tokens_dict:
+                    descripcion = category
+                    break
+            
+            result_lexema.append((descripcion.upper(), token.value, token.lineno))
+        
         return render_template('index.html', tokens=result_lexema)
     return render_template('index.html', tokens=None)
 
