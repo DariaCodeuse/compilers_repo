5960db22925b484a9069601d67f3be27f49b2d91
Karla Díaz <diazaguilarkarla8@gmail.com>
2024-10-01 19:40:31 -0600

    refactor: :art: rewrite productions rules (for)

---
+++ analizador/syntax.py
@@ -0,0 +1,40 @@
+import ply.yacc as yacc
+from lex import tokens, lexer
+
+errors = []
+
+def p_for_statement(p):
+  '''for_statement : FOR LPAREN initialization SEMICOLON condition SEMICOLON update RPAREN LBRACE body RBRACE '''
+  if not errors:
+        print("Estructura del bucle 'for' correcta")
+
+def p_initialization(p):
+  '''initialization : INT IDENTIFIER EQUALS NUMBER '''
+
+def p_condition(p):
+  '''condition : IDENTIFIER LESS_EQUAL NUMBER '''
+
+def p_update(p):
+  '''update : IDENTIFIER INCREMENT
+            | INCREMENT IDENTIFIER '''
+
+def p_body(p):
+  '''body : SYSTEM DOT OUT DOT PRINTLN LPAREN STRING PLUS IDENTIFIER RPAREN SEMICOLON '''
+
+# Error rule for syntax errors
+def p_error(p):
+    if p:
+        errors.append(f"Error de sintaxis en el token '{p.value}', línea {p.lineno}")
+    else:
+        errors.append("Error de sintaxis al final del archivo")
+
+# Build the parser
+parser = yacc.yacc(debug=False)
+
+
+def parser_for_statement(code):
+    global errors
+    errors.clear()  # Limpiar errores previos
+    lexer.lineno = 1
+    parser.parse(code, lexer=lexer)
+    return errors
