8d59d377c45a65f7578fcc6236bc038c09b02056
Karla Díaz <diazaguilarkarla8@gmail.com>
2024-10-08 03:01:38 -0600

    fix: :bug: different types of erros for syntax

--- 2.5_analyzerLexSyn/syntax.py
+++ 2.5_analyzerLexSyn/syntax.py
@@ -3,44 +3,64 @@ from lex import tokens, lexer
 
 errors = []
 succes = []
+check = []
+check2 = []
 
 def p_funcion(p):
     '''funcion : PROGRAMA IDENTIFICADOR LPAREN RPAREN LBRACE estructura RBRACE'''
-    if not errors:
-        print("Ningun error sintáctico!")
 
 def p_estructura(p):
     'estructura : variables entrada operacion impresion salida'
-    pass  # Aquí podrías agregar lógica para almacenar declaraciones
+    # Aquí podrías agregar lógica para almacenar declaraciones
+    pass
 
 def p_variables(p):
-    '''variables : INT IDENTIFICADOR COMA IDENTIFICADOR COMA IDENTIFICADOR SEMICOLON '''
-    print("Variables correctamente declaradas ")
+    '''variables : INT VARIABLE COMA VARIABLE COMA VARIABLE SEMICOLON '''
+    for i in p[2], p[4], p[6]:
+        check.append(i)
 
 def p_entrada(p):
     '''entrada : lectura lectura'''
 
 def p_lectura(p):
-    '''lectura : READ IDENTIFICADOR SEMICOLON'''
+    '''lectura : READ VARIABLE SEMICOLON'''
+    if p[2] not in check:
+        errors.append(f"Variable '{p[2]}' no declarada antes")
+    else:
+        check2.append(p[2])
 
 def p_operacion(p):
-    '''operacion : IDENTIFICADOR IGUAL IDENTIFICADOR ADICION IDENTIFICADOR SEMICOLON'''
+    '''operacion : VARIABLE IGUAL VARIABLE ADICION VARIABLE SEMICOLON'''
+    if p[1] not in check:
+        errors.append(f"Variable '{p[1 ]}' no declarada antes")
+    
+    if p[3] not in check:
+        errors.append(f"Variable '{p[3]}' no declarada antes")
+    elif p[3] not in check2:
+        errors.append(f"Variable '{p[3]}' no leída antes")
+
+    if p[5] not in check:
+        errors.append(f"Variable '{p[5]}' no declarada antes")
+    elif p[5] not in check2:
+        errors.append(f"Variable '{p[5]}' no leída antes")
 
 def p_impresion(p):
-    '''impresion : PRINT LPAREN CADENA RPAREN'''
+    '''impresion : PRINT LPAREN COMILLA CADENA CADENA CADENA COMILLA RPAREN'''
 
 def p_salida(p):
     '''salida : END SEMICOLON'''
 
 def p_error(p):
     if p:
-        errors.append(f"Error de sintaxis en el token '{p.value}', línea {p.lineno}")
+        if p.type == "CADENA":
+            pass 
+        else:
+            errors.append(f"Error de sintaxis en el token '{p.value}', línea {p.lineno}")
     else:
         errors.append("Error de sintaxis al final del archivo")
 
 # Build the parser
-parser = yacc.yacc(debug=False)
-
+parser = yacc.yacc()
 
 def parser_for_statement(code):
     global errors
