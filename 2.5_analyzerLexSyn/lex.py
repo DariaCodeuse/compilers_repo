3396c6a22342f2030da1dc3342574113e9bd3fdb
Karla Díaz <diazaguilarkarla8@gmail.com>
2024-10-08 02:45:28 -0600

    refactor: :fire: added dict copy/ X mark for tokens category/ fix tokens categories

--- 2.5_analyzerLexSyn/lex.py
+++ 2.5_analyzerLexSyn/lex.py
@@ -1,10 +1,19 @@
 import ply.lex as lex
 import json
+import copy
 
 # Cargar el JSON
 with open('tokens.json') as f:
     data = json.load(f)         # Para hacer tokens
-    dataTok = json.load(f)      # Para agregar variables y cadenas
+dataTok = copy.deepcopy(data)     # Para agregar variables y cadenas
+
+dataTok["variable"]["a"] = "VARIABLE"
+dataTok["variable"]["b"] = "VARIABLE"
+dataTok["variable"]["c"] = "VARIABLE"
+
+dataTok["cadena"]["la"] = "CADENA"
+dataTok["cadena"]["suma"] = "CADENA"
+dataTok["cadena"]["es"] = "CADENA"
 
 # Inicializar una lista de categorías (los diccionarios dentro de data)
 lista_categorias = []
@@ -18,7 +27,7 @@ identificador = data["identificador"]
 
 # List of token names (Always required)
 tokens = [token for category in data.values() for token in category.values()]
-tokens += ["IDENTIFICADOR", "VARIABLE", "CADENA", "RESERVADA", "COMILLA"]
+tokens += ["VARIABLE", "CADENA", "COMILLA"]
 
 # Regular expression rules for simple tokens
 t_LPAREN  = r'\('
@@ -29,11 +38,11 @@ t_COMA= r'\,'
 t_SEMICOLON = r';'
 t_ADICION = r'\+'
 t_IGUAL   = r'='
+t_PROGRAMA = r'PROGRAMA'
 
-# A regular expression rule with some action code
+# A regular expression rule with some action code}
 def t_IDENTIFICADOR(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reservada.get(t.value, 'IDENTIFICADOR')
+    r'Suma'
     return t
 
 # Identificadores
@@ -45,23 +54,7 @@ def t_VARIABLE(t):
         t.type = 'CADENA'
     else:
         t.type = 'VARIABLE'
-    r'\w+'
-    if t.value in reservada.keys():
-        t.type = reservada[t.value]
-    elif len(t.value) > 1:
-        t.type = 'CADENA'
-    else:
-        t.type = 'VARIABLE'
     return t
-
-# def t_VARIABLE(t):
-#     r'[a-zA-Z_][a-zA-Z0-9_]*'
-#     if t.value not in reservada and t.value not in identificador:
-#         t.type = 'VARIABLE'  # Marca como variable
-#         identificador[t.value] = t.value  # Registra la nueva variable
-#     else:
-#         t.type = reservada.get(t.value, 'IDENTIFICADOR')
-#     return t
 
 # Track line numbers
 def t_newline(t):
@@ -83,24 +76,7 @@ def t_error(t):
 lexer = lex.lex()
 
 # SOLO PARA TESTEO
-data = '''
-programa suma(){
-    int a,b,c;
-    read a;
-    read b;
-    c = a+b;
-    print("la suma es")
-    end;
-}
-'''
 
-# lexer.input(data)
-# # Tokenize
-# while True:
-#     tok = lexer.token()
-#     if not tok:
-#         break      # No more input
-#     print(tok.type, tok.value, tok.lineno, tok.lexpos)
 
 def all_tokens(input):
     # Inicializar el número de línea en 1
@@ -111,5 +87,25 @@ def all_tokens(input):
         tok = lexer.token()
         if not tok:
             break
-        tokens.append((tok.type, tok.value, tok.lineno))
+
+        lin = tok.lineno
+        tv = tok.value
+        pr = "X" if tok.type in reservada.values() else ""
+        iD = "X" if tok.type == "IDENTIFICADOR" else ""
+        var = "X" if tok.type == "VARIABLE" else ""
+        nu = "X" if tok.type == "NUMERO" else ""
+        cad = "X" if tok.type == "CADENA" else ""
+        sim = "X" if tok.type in data['simbolo'].values() else ""
+        
+        tipo = "Desconocido"
+            # nombre de dict #diccionario
+        for categoria, tokens_categoria in dataTok.items():
+            print(tokens_categoria)
+            if tok.type in tokens_categoria.values():
+                tipo = categoria.capitalize()  # Capitaliza el nombre de la categoría
+                break
+
+        tokens.append((lin, tv, pr, iD, var, nu, cad, sim, tipo))
     return tokens
+
+### quieres que el data te sirva para identificar a que catagoria pertenece cada token
