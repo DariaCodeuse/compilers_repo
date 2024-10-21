d533d7ed2e2cd3dcaf1305a5c769eb5837de2a1b
Karla Díaz <diazaguilarkarla8@gmail.com>
2024-10-21 04:31:40 -0600

    build: :technologist: create some react components
    Body, Calculator, Sidebar, Table
    
    All of them were organized

---
+++ calculadora/frontend/src/components/Body/Body.jsx
@@ -0,0 +1,12 @@
+import Calculator from '../Calculator/Calculator';
+import './Body.css'
+
+export default function Body () {
+
+  return(
+    <div className="body">
+      <Calculator/>
+    </div>
+  )
+
+}
